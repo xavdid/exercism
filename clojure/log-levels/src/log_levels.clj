4c760a05b6c928af4a9685a49e2af36e8cd823cb
David Brownman <beamneocube@gmail.com>
2023-06-03 20:19:41 -0700

    clojure: log-levels

---
+++ clojure/log-levels/src/log_levels.clj
@@ -0,0 +1,26 @@
+(ns log-levels
+  (:require [clojure.string :as str]))
+
+(defn message
+  "Takes a string representing a log line
+   and returns its message with whitespace trimmed."
+  [s]
+  (-> s
+      (str/split #":")
+      last
+      str/trim))
+
+(defn log-level
+  "Takes a string representing a log line
+   and returns its level in lower-case."
+  [s]
+  (->> s
+       (re-find #"\[(.*)\]")
+       last
+       str/lower-case))
+
+
+(defn reformat
+  "Takes a string representing a log line and formats it
+   with the message first and the log level in parentheses."
+  [s] (format "%s (%s)" (message s) (log-level s)))
