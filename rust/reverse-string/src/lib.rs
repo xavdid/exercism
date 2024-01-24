6267af4235ee1fa1272fbb55f9128bf68037c497
David Brownman <beamneocube@gmail.com>
2024-01-24 00:00:58 -0800

    rust: reverse-string

---
+++ rust/reverse-string/src/lib.rs
@@ -0,0 +1,9 @@
+pub fn reverse(input: &str) -> String {
+    String::from(input).chars().rev().collect()
+}
+
+// use unicode_segmentation::UnicodeSegmentation;
+
+// pub fn reverse(input: &str) -> String {
+//     input.graphemes(true).rev().collect()
+// }
